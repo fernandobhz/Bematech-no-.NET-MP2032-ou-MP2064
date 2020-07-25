6d7a264df1106cc90302517be1603429f14fb392
Fernando Reis Guimarães <fernandobhz@gmail.com>
2020-07-25 18:56:59 -0300

    old project migrated

---
+++ Bematech no .NET MP2032 ou MP2064/Bematech.vb
@@ -0,0 +1,61 @@
+﻿Imports System.Runtime.InteropServices
+
+'Public Class MP2032
+
+'    'Configura o modelo da impressora
+'    <DllImport("MP2032.dll")> _
+'    Public Shared Function ConfiguraModeloImpressora(modelo As Integer) As Integer
+'    End Function
+
+'    'Inicia Porta
+'    <DllImport("MP2032.dll")> _
+'    Public Shared Function IniciaPorta(porta As [String]) As Integer
+'    End Function
+
+'    'Enviar texto formatado
+'    <DllImport("MP2032.dll")> _
+'    Public Shared Function FormataTX(texto As [String], TipoLetra As Integer, italico As Integer, sublinhado As Integer, expandido As Integer, enfatizado As Integer) As Integer
+'    End Function
+
+'    'Enviar comandos para a impressora
+'    <DllImport("MP2032.dll")> _
+'    Public Shared Function ComandoTX(comando As [String], tComando As Integer) As Integer
+'    End Function
+
+'    'Fecha a porta
+'    <DllImport("MP2032.dll")> _
+'    Public Shared Function FechaPorta() As Integer
+'    End Function
+
+'End Class
+
+
+Public Class MP2064
+
+    'Configura o modelo da impressora
+    <DllImport("MP2064.dll")> _
+    Public Shared Function ConfiguraModeloImpressora(modelo As Integer) As Integer
+    End Function
+
+
+    'Inicia Porta
+    <DllImport("MP2064.dll")> _
+    Public Shared Function IniciaPorta(porta As [String]) As Integer
+    End Function
+
+    'Enviar texto formatado
+    <DllImport("MP2064.dll")> _
+    Public Shared Function FormataTX(texto As [String], TipoLetra As Integer, italico As Integer, sublinhado As Integer, expandido As Integer, enfatizado As Integer) As Integer
+    End Function
+
+    'Enviar comandos para a impressora
+    <DllImport("MP2064.dll")> _
+    Public Shared Function ComandoTX(comando As [String], tComando As Integer) As Integer
+    End Function
+
+    'Fecha a porta
+    <DllImport("MP2064.dll")> _
+    Public Shared Function FechaPorta() As Integer
+    End Function
+
+End Class
---
+++ Bematech no .NET MP2032 ou MP2064/Form1.vb
@@ -0,0 +1,30 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+
+        Dim iRetorno As Integer = 0
+
+        'Select Case Runtime.InteropServices.Marshal.SizeOf(GetType(IntPtr)) * 8
+        '    Case 32
+        '        iRetorno = MP2032.ConfiguraModeloImpressora(7)
+        '        iRetorno = MP2032.IniciaPorta("COM1")
+        '        iRetorno = MP2032.FormataTX("Bematech – BSP", 2, 0, 0, 1, 1)
+        '        Dim sComando As String = Chr(13) + Chr(10)
+        '        iRetorno = MP2032.ComandoTX(sComando, Len(sComando))
+        '        iRetorno = MP2032.FechaPorta()
+        '        MsgBox("OK")
+        '    Case 64
+        iRetorno = MP2064.ConfiguraModeloImpressora(7)
+        iRetorno = MP2064.IniciaPorta("COM1")
+        iRetorno = MP2064.FormataTX("Bematech – BSP", 2, 0, 0, 1, 1)
+        Dim sComando As String = Chr(13) + Chr(10)
+        iRetorno = MP2064.ComandoTX(sComando, Len(sComando))
+        iRetorno = MP2064.FechaPorta()
+        MsgBox("OK")
+        '    Case Else
+        'Throw New Exception("Arquitetura desconhecida")
+        'End Select
+
+    End Sub
+
+End Class
